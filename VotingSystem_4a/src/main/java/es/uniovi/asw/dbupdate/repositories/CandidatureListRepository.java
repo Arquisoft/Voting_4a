61b34acb99468b8ce02173b0a29768831128ba17
Victor Castaño Gutierrez <victorcastanogutierrez@gmail.com>
2016-05-03 12:55:14 +0200

    Pagina web de partida

--- VotingSystem_4a/src/main/java/es/uniovi/asw/dbupdate/repositories/CandidatureListRepository.java
+++
@@ -1,8 +0,0 @@
-package es.uniovi.asw.dbupdate.repositories;
-
-/**
- * CandidatureListRepository Created by ivan on 2/04/16.
- */
-public interface CandidatureListRepository extends CandidatureRepository {
-
-}
