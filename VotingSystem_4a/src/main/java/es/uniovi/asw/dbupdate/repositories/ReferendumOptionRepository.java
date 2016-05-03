4835acf688ffc94c396527371a52e22580ac7ba1
Lucas García <UO238641@uniovi.es>
2016-05-03 14:23:53 +0200

    Rama con diagramas

--- VotingSystem_4a/src/main/java/es/uniovi/asw/dbupdate/repositories/ReferendumOptionRepository.java
+++
@@ -1,8 +0,0 @@
-package es.uniovi.asw.dbupdate.repositories;
-
-/**
- * ReferendumOptionRepository Created by ivan on 2/04/16.
- */
-public interface ReferendumOptionRepository extends CandidatureRepository {
-
-}
--- censuses_4a/src/main/java/es/uniovi/asw/parser/ReadCensus.java
+++
@@ -1,9 +0,0 @@
-package es.uniovi.asw.parser;
-
-import java.io.IOException;
-
-public interface ReadCensus {
-	
-	void read() throws IOException;
-	
-}
