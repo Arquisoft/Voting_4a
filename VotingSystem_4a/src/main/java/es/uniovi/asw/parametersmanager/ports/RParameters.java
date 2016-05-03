d3466e1dc8017c06bbd7ff942fbe02fe6f767730
Iván <ivan_ma@telecable.es>
2016-05-03 12:10:16 +0200

    Añadida la implementación de la interfaz y los métodos

--- VotingSystem_4a/src/main/java/es/uniovi/asw/parametersmanager/ports/RParameters.java
+++ VotingSystem_4a/src/main/java/es/uniovi/asw/parametersmanager/ports/RParameters.java
@@ -1,18 +1,26 @@
 package es.uniovi.asw.parametersmanager.ports;
 
+import es.uniovi.asw.dbupdate.ports.GetParametersP;
+import es.uniovi.asw.model.ElectionCall;
+import es.uniovi.asw.util.ParametersException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import es.uniovi.asw.model.types.ElectionDateTime;
 import es.uniovi.asw.parametersmanager.ReadParameters;
 
 /**
- * RParameters Created by ivan on 2/04/16.
+ * RParameters
+ * Created by ivan on 2/04/16.
  */
 @Service
 public class RParameters implements ReadParameters {
 
-	public void read(ElectionDateTime edt, String name, String description) {
+	@Autowired
+	private GetParametersP getParametersP;
 
+	@Override
+	public Iterable<ElectionCall> getElectionCalls() throws ParametersException {
+		return getParametersP.getElectionCalls();
 	}
 
 }
