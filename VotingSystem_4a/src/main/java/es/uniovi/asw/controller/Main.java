191314d09ffa43b09e38f6811d56ae5bf8083a47
Iván <ivan_ma@telecable.es>
2016-05-03 11:59:44 +0200

    Añadidos los métodos de mapeo

--- VotingSystem_4a/src/main/java/es/uniovi/asw/controller/Main.java
+++ VotingSystem_4a/src/main/java/es/uniovi/asw/controller/Main.java
@@ -1,11 +1,15 @@
 package es.uniovi.asw.controller;
 
-import java.util.Collection;
-import java.util.List;
-
-import es.uniovi.asw.dbupdate.ports.GetParametersP;
+import es.uniovi.asw.model.Election;
+import es.uniovi.asw.model.ElectionCall;
+import es.uniovi.asw.model.ReferendumOption;
+import es.uniovi.asw.parametersmanager.ports.InsertR;
+import es.uniovi.asw.parametersmanager.ports.RParameters;
 import es.uniovi.asw.util.Login;
 import es.uniovi.asw.util.ParametersException;
+import es.uniovi.asw.votingmanager.ports.GetParametersR;
+import es.uniovi.asw.votingmanager.ports.RCheckVoter;
+import es.uniovi.asw.votingmanager.ports.RVote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,43 +21,170 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import es.uniovi.asw.dbupdate.repositories.ElectionRepository;
-import es.uniovi.asw.dbupdate.repositories.VoterRepository;
-import es.uniovi.asw.model.Candidature;
-import es.uniovi.asw.model.District;
-import es.uniovi.asw.model.Region;
 import es.uniovi.asw.model.Voter;
 
 @Controller
 public class Main {
 
 	@Autowired
-	private ElectionRepository electionRepository;
+	private Login login;
 
 	@Autowired
-	private GetParametersP getParametersP;
-	
+	private RParameters rParameters;
+
 	@Autowired
-	private VoterRepository voter;
+	private InsertR insertR;
+
+	@Autowired
+	private GetParametersR getParametersR;
+
+	@Autowired
+	private RCheckVoter rCheckVoter;
+
+	@Autowired
+	private RVote rVote;
 
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView landing(Model model) {
+	public ModelAndView index(Model model) {
 		logger.info("Index page access");
 		model.addAttribute("voter", new Voter());
 
 		return new ModelAndView("index");
 	}
 
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public ModelAndView about(Model model) {
+		logger.info("About Us page access");
+		model.addAttribute("voter", new Voter());
+
+		return new ModelAndView("about-us");
+	}
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public ModelAndView adminIndex(Model model) {
+		logger.info("Admin page access");
+		model.addAttribute("voter", new Voter());
+
+		return new ModelAndView("index-electoral-board");
+	}
+
+	@RequestMapping(value = "/check-voter", method = RequestMethod.GET)
+	public ModelAndView checkVoter(Model model) {
+		logger.info("Check voter access");
+		model.addAttribute("voter", new Voter());
+
+		return new ModelAndView("check-voter");
+	}
+
+	@RequestMapping(value = "/check-voter", method = RequestMethod.POST)
+	public ModelAndView checkVoterVoted(@RequestParam(value = "voterEmail") String email, Model model) {
+		logger.info("Check voter voted access");
+
+		try {
+
+			boolean voted = rCheckVoter.hasVoted(email);
+			model.addAttribute("email", email);
+			model.addAttribute("voted", voted);
+
+		}
+
+		catch (ParametersException e) {
+			logger.info("ERROR check-voter: " + e.getMessage());
+			model.addAttribute("error", e.getMessage());
+		}
+
+		return new ModelAndView("check-voter");
+	}
+
+	@RequestMapping(value = "/vote/{id}/{voter}/{votingPlace}", method = RequestMethod.GET)
+	public ModelAndView Vote(@PathVariable Long id, @PathVariable Long voter, @PathVariable Long votingPlace, Model model) {
+		logger.info("Vote access");
+
+		try {
+
+			Election election = getParametersR.getReferendum(id);
+			model.addAttribute("election", election);
+
+			Iterable<ReferendumOption> referendumOptions = getParametersR.getReferendumOptions(election.getId());
+			model.addAttribute("referendumOptions", referendumOptions);
+
+			model.addAttribute("voter", voter);
+			model.addAttribute("votingPlace", votingPlace);
+
+		}
+
+		catch (ParametersException e) {
+			logger.info("ERROR vote: " + e.getMessage());
+			model.addAttribute("error", e.getMessage());
+		}
+
+		return new ModelAndView("vote");
+	}
+
+	@RequestMapping(value = "/vote", method = RequestMethod.POST)
+	public ModelAndView RegisterVote(@RequestParam(value = "voter") Long voter, @RequestParam(value = "votingPlace") Long votingPlace, @RequestParam(value = "id") Long id, Model model) {
+		logger.info("Vote");
+
+		try {
+
+			rVote.registerVote(voter, votingPlace, id);
+			model.addAttribute("error", "El voto ha sido contabilizado");
+
+		}
+
+		catch (ParametersException e) {
+			logger.info("ERROR vote: " + e.getMessage());
+			model.addAttribute("error", e.getMessage());
+		}
+
+		return new ModelAndView("index-voter");
+	}
+
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public ModelAndView newElection(Model model) {
+		logger.info("New Election page access");
+		model.addAttribute("electionCall", new ElectionCall());
+
+		return new ModelAndView("new-election-phase1");
+	}
+
+	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	public ModelAndView newElection(@RequestParam(value = "electionName") String electionName,
+	                                @RequestParam(value = "electionDesc") String electionDesc,
+	                                @RequestParam(value = "dateStart") String dateStart,
+	                                @RequestParam(value = "dateEnd") String dateEnd,
+	                                @RequestParam(value = "electionQuestion") String electionQuestion,
+	                                @RequestParam(value = "electionResp1") String electionResp1,
+	                                @RequestParam(value = "electionResp2") String electionResp2, Model model) {
+
+		logger.info("Create Election access");
+
+		try {
+
+			insertR.createReferendum(electionName, electionDesc, dateStart, dateEnd, electionQuestion, electionResp1, electionResp2);
+			model.addAttribute("error", "Referéndum creado correctamente");
+
+		}
+
+		catch (Exception e) {
+			logger.info("ERROR newElection: " + e.getMessage());
+			model.addAttribute("error", e.getMessage());
+		}
+
+		return new ModelAndView("new-election-phase1");
+	}
+
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public ModelAndView login(@RequestParam(value = "email") String email,
-			@RequestParam(value = "password") String password, Model model) {
+	public ModelAndView login(@RequestParam(value = "lg_username") String email,
+	                          @RequestParam(value = "lg_password") String password, Model model) {
+
 		logger.info("Login");
 
 		try {
-			Login login = new Login();
-			Object loginData = login.auth(voter,email, password);
+
+			Object loginData = login.auth(email, password);
 
 			if (loginData == null) {
 				logger.info("User/pass incorrect");
@@ -64,7 +195,7 @@ public class Main {
 			else if (loginData instanceof Voter) {
 				logger.info("Login Usuario " + ((Voter) loginData).getName());
 				model.addAttribute("voter", loginData);
-				model.addAttribute("elecciones", getParametersP.getElections(1L));
+				model.addAttribute("elecciones", rParameters.getElectionCalls());
 				return new ModelAndView("index-voter");
 			}
 
@@ -84,7 +215,7 @@ public class Main {
 
 	}
 
-	@RequestMapping("candidaturas/{id}")
+	/*@RequestMapping("candidaturas/{id}")
 	public String showCandidaturas(@PathVariable Long id, Model model) {
 
 		Region r = (Region) electionRepository.findOne(id).getRegions().toArray()[0];
@@ -93,6 +224,6 @@ public class Main {
 		model.addAttribute("candidaturas", candidaturas);
 
 		return "candidaturas";
-	}
+	}*/
 
 }
